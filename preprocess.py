9b4a93d66371b51500e6f9a5ce647ada885e5f68
Martina Toshevska <martina.tosevska.95@gmail.com>
2019-04-16 00:06:45 +0200

    Implement lemmatization

--- preprocess.py
+++ preprocess.py
@@ -4,6 +4,10 @@ from collections import Counter
 import pandas as pd
 import _pickle as pickle
 import numpy as np
+from nltk.corpus import stopwords
+from string import punctuation
+from lemmatization import lemmatize
+from tqdm import tqdm
 
 
 def tokenize_review(review):
@@ -15,7 +19,8 @@ def parse_json_review(review):
     review_json = json.loads(review)
     review_id = review_json['review_id']
     review_text = tokenize_review(review_json['text'])
-    return {'Review_ID': review_id, 'Text': review_text}
+    # return {'Review_ID': review_id, 'Text': review_text}
+    return review_id, review_text
 
 
 def filter_reviews(file_name):
@@ -53,19 +58,31 @@ def filter_reviews(file_name):
     print('Percentage 10-500: ' + str(filtered_10_500 * 100.0 / total))
 
 
-def tokenize(file_name, r1, r2):
-    reviews = list()
-    with open(file_name, 'r', encoding='utf-8') as doc_r:
+def tokenize_reviews(r1, r2):
+    reviews = dict()
+    with open(f'data/yelp_reviews_filtered_{r1}_{r2}.json', 'r', encoding='utf-8') as doc_r:
         line = doc_r.readline()
         i = 0
         while line != '':
             i += 1
             if i % 100000 == 0:
                 print(str(i))
-            review = parse_json_review(line)
-            reviews.append(review)
+            review_id, review_text = parse_json_review(line)
+            reviews[review_id] = review_text
             line = doc_r.readline()
     with open(f'data/yelp_reviews_tokens_{r1}_{r2}.pkl', 'wb') as doc_w:
+        pickle.dump(reviews, doc_w)
+
+
+def lemmatize_reviews(r1, r2):
+    with open(f'data/yelp_reviews_tokens_{r1}_{r2}.pkl', 'rb') as doc_r:
+        data = pickle.load(doc_r)
+    reviews = dict()
+    for _, review_id in zip(tqdm(list(range(len(list(data.keys()))))), list(data.keys())):
+        review_tokens = data[review_id]
+        review_lemmas = lemmatize(review_tokens)
+        reviews[review_id] = review_lemmas
+    with open(f'data/yelp_reviews_lemmas_{r1}_{r2}.pkl', 'wb') as doc_w:
         pickle.dump(reviews, doc_w)
 
 
@@ -88,25 +105,23 @@ def create_reviews_subset(file_name):
 
 
 def filter_vocabulary(vocab, frequencies):
-    ...
+    allowed_tokens = [f[0] for f in frequencies if f[1] > 15]
+    return [w for w in vocab if w in allowed_tokens and w not in stopwords.words('english') and w not in punctuation]
 
 
-def create_vocabulary(file_name, r1, r2):
+def create_vocabulary(r1, r2):
+    with open(f'data/yelp_reviews_lemmas_{r1}_{r2}.pkl', 'rb') as doc_r:
+        data = pickle.load(doc_r)
     vocabulary = list()
-    with open(file_name, 'r', encoding='utf-8') as doc:
-        line = doc.readline()
-        i = 0
-        while line != '':
-            i += 1
-            if i % 100000 == 0:
-                print(str(i))
-            review = json.loads(line)
-            vocabulary.extend(tokenize_review(review['text']))
-            line = doc.readline()
+    for _, review_id in zip(tqdm(list(range(len(list(data.keys()))))), list(data.keys())):
+        review_lemmas = data[review_id]
+        vocabulary.extend(review_lemmas)
     pd.DataFrame(list(set(vocabulary))).to_csv(f'data/vocabulary_full_{str(r1)}_{str(r2)}.csv')
     frequencies = sorted(Counter(list(vocabulary)).items(), key=lambda x: x[1], reverse=True)
     with open(f'data/vocabulary_full_frequencies_{str(r1)}_{str(r2)}.pkl', 'wb') as f:
         pickle.dump(frequencies, f)
+    filtered_vocabulary = filter_vocabulary(list(set(vocabulary)), frequencies)
+    pd.DataFrame(list(set(filtered_vocabulary))).to_csv(f'data/vocabulary_{str(r1)}_{str(r2)}.csv')
 
 
 def create_user_review_frequencies(file_name):
@@ -148,7 +163,9 @@ if __name__ == '__main__':
     # create_user_review_frequencies('data/yelp_reviews.json')
     # calculate_reviews_length('data/yelp_reviews.json')
     # filter_reviews('data/yelp_reviews.json')
-    # create_vocabulary('data/yelp_reviews_filtered_50_500.json', 50, 500)
-    # create_vocabulary('data/yelp_reviews_filtered_10_500.json', 10, 500)
-    tokenize('data/yelp_reviews_filtered_50_500.json', 50, 500)
-    tokenize('data/yelp_reviews_filtered_10_500.json', 10, 500)
+    # tokenize_reviews(50, 500)
+    # tokenize_reviews(10, 500)
+    # lemmatize_reviews(50, 500)
+    # lemmatize_reviews(10, 500)
+    create_vocabulary(50, 500)
+    create_vocabulary(10, 500)
